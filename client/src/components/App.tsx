164ee04a70132450f725bf35d3bca77aaacff2ce
danzel ong <danzelongwj01@gmail.com>
2024-08-01 17:45:45 +0800

    client: implemented light & dark mode feat

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from '../routes/LandingPage';
 import Login from '../routes/Login';
@@ -7,6 +8,23 @@ import Settings from '../routes/Settings';
 import PageNotFound from '../routes/PageNotFound';
 
 const App = () => {
+  const [isDarkTheme, setDarkTheme] = useState<boolean>(() => {
+    const result: string | null = localStorage.getItem('theme');
+
+    if (!result) {
+      const query = window.matchMedia('(prefers-color-scheme: dark)');
+      return query.matches ? true : false;
+    }
+    const theme = JSON.parse(result);
+    return theme === 'dark' ? true : false;
+  });
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDarkTheme);
+    const theme = JSON.stringify(isDarkTheme ? 'dark' : 'light');
+    localStorage.setItem('theme', theme);
+  }, [isDarkTheme]);
+
   return (
     <>
       <BrowserRouter>
@@ -14,7 +32,12 @@ const App = () => {
           <Route path='/' element={<LandingPage />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
-          <Route path='/home' element={<Home />} />
+          <Route
+            path='/home'
+            element={
+              <Home isDarkTheme={isDarkTheme} setDarkTheme={setDarkTheme} />
+            }
+          />
           <Route path='/settings' element={<Settings />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
