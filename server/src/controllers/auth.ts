7e6bfddb58ffe3c84150429ca3346208519998ae
danzel ong <danzelongwj01@gmail.com>
2024-08-04 02:11:55 +0800

    feat (server): implemented signup and login controllers to handle user registration and login

---
+++ server/src/controllers/auth.ts
@@ -0,0 +1,80 @@
+import { sendOTPVerificationMail } from '../utils/mailer';
+import passport, { AuthenticateCallback } from 'passport';
+import { matchedData } from 'express-validator';
+import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
+import { Request, Response, NextFunction } from 'express-serve-static-core';
+import { IVerifyOptions } from 'passport-local';
+import User from '../models/user';
+import OTP from '../models/OTP';
+
+// (POST) registers a user
+export async function signup(req: Request, res: Response, next: NextFunction) {
+  const { username, email, password } = matchedData(req);
+
+  try {
+    const existingUser = await User.findOne({ username }).exec();
+    const existingEmail = await User.findOne({ email }).exec();
+
+    const errorMsgs: { username?: string; email?: string } = {};
+
+    if (existingUser) {
+      errorMsgs.username = 'Username is already taken';
+    }
+
+    if (existingEmail) {
+      errorMsgs.email = 'Email is already taken';
+    }
+
+    if (Object.keys(errorMsgs).length) {
+      return res.status(409).json({ type: 'Conflict Error', errorMsgs });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // create new user
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // create OTP token
+    const { otp } = await OTP.create({
+      email: newUser.email,
+    });
+
+    // send OTP to user's email
+    await sendOTPVerificationMail(newUser, otp);
+    
+    res.sendStatus(201);
+  } catch (error) {
+    return next(createHttpError(500, 'Internal server error'));
+  }
+}
+
+// (POST) logs a user in
+export function login(req: Request, res: Response, next: NextFunction) {
+  const callbackFn: AuthenticateCallback = (err, user, info) => {
+    if (err) {
+      return next(createHttpError(500, 'Internal server error'));
+    }
+
+    if (!user) {
+      return res.status(401).json({
+        type: 'Unauthorized Error',
+        errorMsgs: (info as IVerifyOptions).message,
+      });
+    }
+
+    req.login(user, (err) => {
+      if (err) {
+        return next(createHttpError(500, 'Internal server error'));
+      }
+      res.sendStatus(200);
+    });
+  };
+
+  passport.authenticate('local', callbackFn)(req, res, next);
+}
