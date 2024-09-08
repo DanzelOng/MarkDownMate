738670f1b785404e21dbdf4b389cee79ec1397bb
danzel ong <danzelongwj01@gmail.com>
2024-09-08 18:00:31 +0800

    chore (client): integrated Toaster component for rendering toasts

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import LandingPage from '../routes/LandingPage';
 import Login from '../routes/Login';
 import Signup from '../routes/Signup';
 import Home from '../routes/Home';
 import Settings from '../routes/Settings';
+import PasswordReset from '../routes/PasswordReset';
 import PageNotFound from '../routes/PageNotFound';
 
 const App = () => {
@@ -38,10 +40,12 @@ const App = () => {
               <Home isDarkTheme={isDarkTheme} setDarkTheme={setDarkTheme} />
             }
           />
+          <Route path='password-reset' element={<PasswordReset />} />
           <Route path='/settings' element={<Settings />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster toastOptions={{ className: 'font-roboto' }} />
     </>
   );
 };
